68f905de7a1bd81251f7a91ca75c99a26e8929fc
Jessica (Gaoyuan Liang) <gaoyuanliang@outlook.com>
2020-08-31 21:46:46 +0400

    Create deep_depression_detector_data_visulize.py

---
+++ deep_depression_detector_data_visulize.py
@@ -0,0 +1,182 @@
+import os
+from jessica_local_spark_building import sqlContext
+from elasticsearch import *
+es=Elasticsearch([{'host':'localhost','port':9200}])
+
+condition_folder = list(os.walk("data/condition"))[0]
+control_folder = list(os.walk("data/control"))[0]
+
+data_files = ["%s/%s"%(condition_folder[0],f) for f in condition_folder[2]] + ["%s/%s"%(control_folder[0],f) for f in control_folder[2]]
+
+file_idx = 0
+for f in data_files:
+	df = sqlContext.read.format('csv').option('header', 'true').load(f)
+	df.registerTempTable('data')
+	df = sqlContext.sql(u"""
+		SELECT '%s' AS file_name, * FROM data
+		"""%(f))
+	df.write.mode("Overwrite").json('temp_%d.json'%(file_idx))
+	file_idx = file_idx+1
+
+sqlContext.read.json('temp_*.json').registerTempTable('data')
+
+sqlContext.sql(u"""
+	SELECT *, 
+	SPLIT(file_name, '/')[1] AS label,
+	SPLIT(SPLIT(file_name, '/')[2], '.csv')[0] AS person_id
+	FROM data
+	ORDER BY  activity DESC
+	""").write.mode("Overwrite").json('activity_data.json')
+
+os.system(u"rm -r temp*.json")
+
+sqlContext.read.json('activity_data.json').registerTempTable('activity_data')
+sqlContext.read.format('csv').option('header', 'true').load('data/scores.csv').registerTempTable('scores')
+
+'''
+sqlContext.sql(u"""
+	SELECT 
+	activity_data.activity,
+	activity_data.date,
+	activity_data.file_name,
+	activity_data.label,
+	activity_data.person_id,
+	CONCAT(SPLIT(activity_data.timestamp, ' ')[0], 'T',
+	SPLIT(activity_data.timestamp, ' ')[1],
+	'+00:00') AS timestamp
+	FROM activity_data
+	""").show(100, False)
+
+sqlContext.sql(u"""
+	SELECT 
+	activity_data.*,
+	scores.*
+	FROM activity_data
+	LEFT JOIN scores
+	ON scores.number = activity_data.person_id
+	ORDER BY person_id ASC
+	""").show()
+'''
+
+'''
+It contains the following columns; number (patient identifier), days (number of days of measurements), gender (1 or 2 for female or male), age (age in age groups), afftype (1: bipolar II, 2: unipolar depressive, 3: bipolar I), melanch (1: melancholia, 2: no melancholia), inpatient (1: inpatient, 2: outpatient), edu (education grouped in years), marriage (1: married or cohabiting, 2: single), work (1: working or studying, 2: unemployed/sick leave/pension), madrs1 (MADRS score when measurement started), madrs2 (MADRS when measurement stopped).
+'''
+
+sqlContext.sql(u"""
+	SELECT STRING(HASH(activity_data.*)) AS document_id,
+	INT(activity_data.activity) AS activity,
+	CASE 
+		WHEN activity_data.label = 'condition' 
+		THEN INT(activity_data.activity)
+		ELSE NULL
+	END AS activity_depressed,
+	CASE 
+		WHEN activity_data.label = 'control' THEN INT(activity_data.activity)
+		ELSE NULL
+	END AS activity_nondepressed,
+	activity_data.date,
+	activity_data.file_name,
+	CASE 
+		WHEN activity_data.label = 'condition' THEN 'depressed'
+		WHEN activity_data.label = 'control' THEN 'nondepressed'
+		ELSE NULL
+	END AS label,
+	activity_data.person_id,
+	CONCAT(SPLIT(activity_data.timestamp, ' ')[0], 'T',
+	SPLIT(activity_data.timestamp, ' ')[1],
+	'+00:00') AS timestamp,
+	INT(SUBSTR(activity_data.timestamp, 12,2)) AS activity_hour,
+	scores.number,
+	INT(scores.days) AS days,
+	CASE
+		WHEN scores.gender = '1' THEN 'female' ELSE 'male'
+	END AS gender,
+	scores.age,
+	CASE 
+		WHEN scores.afftype = '1' THEN 'bipolar II'
+		WHEN scores.afftype = '2' THEN 'unipolar depressive'
+		WHEN scores.afftype = '3' THEN 'bipolar I'
+		ELSE 'non-depressed'
+	END AS afftype,
+	CASE 
+		WHEN scores.melanch = '1' THEN 'melancholia'
+		WHEN scores.melanch = '2' THEN 'no melancholia'
+		ELSE NULL
+	END AS melanch,
+	CASE 
+		WHEN scores.inpatient = '1' THEN 'inpatient'
+		WHEN scores.inpatient = '2' THEN 'outpatient'
+		ELSE 'non-inpatient'
+	END AS inpatient,
+	scores.edu,
+	CASE 
+		WHEN scores.marriage = '1' THEN 'married or cohabiting'
+		WHEN scores.marriage = '2' THEN 'single'
+		ELSE  'n/a'
+	END AS marriage,
+	CASE 
+		WHEN scores.work = '1' THEN 'working or studying'
+		WHEN scores.work = '2' THEN 'unemployed/sick leave/pension'
+		ELSE  'n/a'
+	END AS work,
+	INT(scores.madrs1) AS madrs1,
+	INT(scores.madrs2) AS madrs2
+	FROM activity_data
+	LEFT JOIN scores
+	ON scores.number = activity_data.person_id
+	""").write.mode("Overwrite").json('activity_data_score.json')
+
+df = sqlContext.read.json('activity_data_score.json')
+df.registerTempTable('activity_data_score')
+
+'''
++--------+
+|count(1)|
++--------+
+| 1571706|
++--------+
+
+sqlContext.sql(u"""
+	SELECT * FROM activity_data_score
+	WHERE label = 'control'
+	ORDER BY edu DESC
+	""").show()
+
+sqlContext.sql(u"""
+	SELECT * FROM activity_data_score
+	ORDER BY melanch DESC
+	""").show()
+'''
+
+
+'''
+DELETE /activity_data_score
+PUT /activity_data_score
+'''
+
+
+'''
+data = df.collect()
+data = sqlContext.sql(u"""
+	SELECT * FROM activity_data_score
+	WHERE person_id IN ('condition_20', 
+	'control_11', 'condition_13',
+	'control_1', 'condition_1')
+	""").collect()
+'''
+
+df1 = sqlContext.sql(u"""
+	SELECT * FROM activity_data_score
+	ORDER BY person_id ASC
+	""")
+data = df1.collect()
+
+for i in data:
+	try:
+		r = i.asDict()
+		es.index(index='activity_data_score',
+		doc_type='activity',
+		id=r['document_id'],
+		body=r)
+	except:
+		pass
